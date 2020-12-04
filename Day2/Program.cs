0b62cce716580d2de57a60f0ba831c6c992cec5a
Vlad Gheorghiu <vladghe@gmail.com>
2020-12-04 03:03:02 +0200

    day2/second-challenge

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -44,6 +44,38 @@ namespace Day2
 
             Console.WriteLine("The number of valid passwords is {0}", numberOfValidPasswords);
 
+            // Part 2
+            numberOfValidPasswords = 0;
+
+            foreach (var line in input)
+            {
+                // Split the rules and the password
+                string[] lineSplit = line.Split(':');
+
+                // Split the occurrences from the letter they apply to
+                string[] firstSplit = lineSplit[0].Split(' ');
+
+                // Split the first and second occurrences and parse themtaking in consideration index zero
+                string[] occurrencesSplit = firstSplit[0].Split('-');
+                int firstPosition = Int32.Parse(occurrencesSplit[0]) - 1;
+                int secondPosition = Int32.Parse(occurrencesSplit[1]) - 1;
+
+                // Retain the letter that needs checked
+                char[] letterToCheck = firstSplit[1].ToCharArray();
+
+                // Get rid of the white spaces
+                string simpleString = lineSplit[1].Trim().ToString();
+
+                // If the letter appears on one position but not on both then increment
+                if ((simpleString[firstPosition] == letterToCheck[0] || simpleString[secondPosition] == letterToCheck[0])
+                    && !(simpleString[firstPosition] == letterToCheck[0] && simpleString[secondPosition] == letterToCheck[0]))
+                {
+                    numberOfValidPasswords++;
+                }
+            }
+
+            Console.WriteLine("The number of valid passwords is {0}", numberOfValidPasswords);
+
             // Prevents console app from closing
             Console.WriteLine("Press any key to close program");
             Console.ReadKey();
