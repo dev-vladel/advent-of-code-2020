ada5a0cddc44065f6869e07805a7708a7244dbb5
Vlad Gheorghiu <vladghe@gmail.com>
2020-12-04 23:11:15 +0200

    day3/first-challenge

---
+++ Day3/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+
+namespace Day3
+{
+    public static class Constants
+    {
+        public const string filePath = @"../../../input.txt";
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines(Constants.filePath);
+
+            int rightToCheck = 0;
+            int numberOfTrees = 0;
+            int numberOfNoTrees = 0;
+            
+            foreach (var line in input)
+            {
+                if (line.Length > rightToCheck)
+                {
+                    if (line[rightToCheck] == '#')
+                    {
+                        numberOfTrees++;
+                    } 
+                    else
+                    {
+                        numberOfNoTrees++;
+                    }
+                } 
+                else if (line.Length <= rightToCheck)
+                {
+                    int rightToCheckAux = rightToCheck;
+
+                    while (line.Length <= rightToCheckAux)
+                    {
+                        rightToCheckAux -= line.Length;
+                    }
+
+                    if (line[rightToCheckAux] == '#')
+                    {
+                        numberOfTrees++;
+                    }
+                    else
+                    {
+                        numberOfNoTrees++;
+                    }
+                }
+
+                rightToCheck += 3;
+            }
+
+            Console.WriteLine(string.Format("The number of trees encountered is {0}", numberOfTrees));
+
+            // Prevents console app from closing
+            Console.WriteLine("Press any key to close program");
+            Console.ReadKey();
+        }
+    }
+}
