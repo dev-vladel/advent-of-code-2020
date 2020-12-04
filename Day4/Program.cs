fdf47cce8eb110749e06e469f113cff382ead3a4
Vlad Gheorghiu <vladghe@gmail.com>
2020-12-05 00:26:42 +0200

    day4/first-challenge

---
+++ Day4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace Day4
+{
+    public static class Constants
+    {
+        public const string filePath = @"../../../input.txt";
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines(Constants.filePath);
+
+            //solution to be added
+
+            // Prevents console app from closing
+            Console.WriteLine("Press any key to close program");
+            Console.ReadKey();
+        }
+    }
+}
