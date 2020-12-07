c1b60b791e096f2afa9c79354303e5e4f24c52dc
Vlad Gheorghiu <vladghe@gmail.com>
2020-12-07 03:54:49 +0200

    day6/first-change: solved

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -12,9 +12,35 @@ namespace Day6
     {
         static void Main(string[] args)
         {
+            // I've added another line to the input.txt file because File.ReadAllLines ignores the last line if its whitespace or empty
             string[] input = File.ReadAllLines(Constants.filePath);
 
+            int sum = 0;
+            int[] appearances = new int[130];
+
             // solution to be added
+            foreach (var line in input)
+            {
+                foreach (var character in line)
+                {
+                    appearances[character]++;
+                }
+
+                if (string.IsNullOrEmpty(line))
+                {
+                    foreach(var number in appearances)
+                    {
+                        if (number != 0)
+                        {
+                            sum++;
+                        }
+                    }
+
+                    appearances = new int[130];
+                }
+            }
+
+            Console.WriteLine(string.Format("The sum of all questions is {0}", sum));
 
             // Prevents console app from closing
             Console.WriteLine("Press any key to close program");
