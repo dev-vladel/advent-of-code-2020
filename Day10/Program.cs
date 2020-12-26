3cfdade75b041dfe9cd88e8402705cb9ae0b40a3
Vlad Gheorghiu <vladghe@gmail.com>
2020-12-27 00:48:47 +0200

    day10/second-challenge: added solution

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -31,7 +31,7 @@ namespace Day9
             #endregion
 
             #region part two
-
+            Console.WriteLine("The number of available chains are {0}", PartTwo(parsedInput));
             #endregion
 
             // Prevents console app from closing
@@ -56,6 +56,35 @@ namespace Day9
             Console.WriteLine("One's: {0} | Three's: {1}", differences[1], differences[3]);
 
             return (differences[1] * differences[3]);
+        }
+
+        public static string PartTwo(List<long> input)
+        {
+            // Adding the base jolt
+            input.Add(0);
+            // Adding the maximum jolt
+            input.Add(input.Max() + 3);
+            // Sorting the input
+            input.Sort();
+
+            Dictionary<int, long> memoize = new Dictionary<int, long>()
+            {
+                [input.Count - 1] = 1
+            };
+
+            for (int k = input.Count - 2; k >= 0; k--)
+            {
+                long connections = 0L;
+
+                for (var connected = k + 1; connected < input.Count && input[connected] - input[k] <= 3; connected++)
+                {
+                    connections += memoize[connected];
+                }
+
+                memoize[k] = connections;
+            }
+
+            return memoize[1].ToString();
         }
     }
 }
