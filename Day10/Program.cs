ed08c7731be4d6b9a1c6820f0087b226cfa9d3b2
Vlad Gheorghiu <vladghe@gmail.com>
2020-12-27 00:17:51 +0200

    day10/first-challenge: added better solution

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using MoreLinq;
 
 namespace Day9
 {
@@ -12,40 +15,18 @@ namespace Day9
     {
         static void Main(string[] args)
         {
-            string[] input = File.ReadAllLines(Constants.filePath);
+            string input = File.ReadAllText(Constants.filePath);
 
             #region part one
-            long[] orderedInput = new long[33];
-            int index = 1;
-            orderedInput[0] = 0;
+            var parsedInput = input.Replace("\r", string.Empty)
+                .Split('\n')
+                .Reverse()
+                .SkipWhile(string.IsNullOrEmpty)
+                .Reverse()
+                .Select(s => (long)Convert.ChangeType(s, typeof(long)))
+                .ToList();
 
-            long aux = 0;
-
-            foreach (var line in input)
-            {
-                orderedInput[index] = Convert.ToInt64(line);
-                index++;
-            }
-
-            for (int i = 0; i < orderedInput.Length - 1; i++)
-            {
-                for (int j = i + 1; j < orderedInput.Length; j++)
-                {
-                    if (orderedInput[i] > orderedInput[j])
-                    {
-                        aux = Convert.ToInt64(orderedInput[i]);
-                        orderedInput[i] = orderedInput[j];
-                        orderedInput[j] = aux;
-                    }
-                }
-            }
-
-            orderedInput[index] = orderedInput[index - 1] + 3;
-
-            //orderedInput[index] = orderedInput[index - 1] + 3;
-
-            PartOne(orderedInput);
-
+            Console.WriteLine("One's multiplied by Three's is {0}", PartOne(parsedInput));
             #endregion
 
             #region part two
@@ -57,30 +38,20 @@ namespace Day9
             Console.ReadKey();
         }
 
-        public static void PartOne(long[] input)
+        public static int PartOne(List<long> input)
         {
-            int ones = 0;
-            int twos = 0;
-            int threes = 0;
+            input.Add(0);
+            input.Add(input.Max() + 3);
 
-            for (int i = 0; i < input.Length - 1; i++)
-            {
-                switch (input[i] - input[i+1])
-                {
-                    case -1:
-                        ones++;
-                        break;
-                    case -2:
-                        twos++;
-                        break;
-                    case -3:
-                        threes++;
-                        break;
-                }
-            }
+            var differences = input
+                .OrderBy(x => x)
+                .Pairwise((a, b) => b - a)
+                .GroupBy(x => x)
+                .ToDictionary(x => x.Key, y => y.Count());
 
-            Console.WriteLine("One's: {0} | Two's: {1} | Three's {2}", ones, twos, threes);
-            Console.WriteLine("One's multiplied by Three's is {0}", ones * threes);
+            Console.WriteLine("One's: {0} | Three's: {1}", differences[1], differences[3]);
+
+            return (differences[1] * differences[3]);
         }
     }
 }
