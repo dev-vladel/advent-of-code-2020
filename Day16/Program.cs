d5bce69207e51e5114724abc62bf20143aa66aed
Vlad Gheorghiu <vladghe@gmail.com>
2020-12-26 19:36:30 +0200

    refactoring: changed numbering and added all base projects

---
+++ Day16/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Day9
+{
+    public static class Constants
+    {
+        public const string filePath = @"../../../input.txt";
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines(Constants.filePath);
+
+            #region part one
+
+            #endregion
+
+            #region part two
+
+            #endregion
+
+            // Prevents console app from closing
+            Console.WriteLine("Press any key to close program");
+            Console.ReadKey();
+        }
+    }
+}
