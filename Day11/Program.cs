b2d3c1902ffe463b2ad972b8af93642dc674b8a6
Vlad Gheorghiu <vladghe@gmail.com>
2021-01-12 23:26:42 +0200

    day11/first-solution: cleaned solution

--- Day11/Program.cs
+++ Day11/Program.cs
@@ -6,6 +6,7 @@ namespace Day9
     public static class Constants
     {
         public const string filePath = @"../../../input.txt";
+        public const int x = 0;
     }
 
     class Program
@@ -14,43 +15,26 @@ namespace Day9
         {
             string[] input = File.ReadAllLines(Constants.filePath);
 
-            var matrix = new char[input.Length + 2, input[0].Length + 2];
-            var matrixCopy = new char[input.Length + 2, input[0].Length + 2];
-            var row = 1;
+            var rowLength = input.Length + 2;
+            var columnLength = input[0].Length + 2;
+
+            var matrix = new char[rowLength, columnLength];
+            var matrixCopy = new char[rowLength, columnLength];
             var countSeats = 0;
             var run = true;
 
             // Building matrix
-
-            // Building outer layer
-            for (int i = 0; i < input.Length + 2; i++)
-            {
-                for (int j = 0; j < input[0].Length + 2; j++)
-                {
-                    matrix[i, j] = '.';
-                }
-            }
-
-            // Building inner layer
-            foreach (var line in input)
-            {
-                for (int j = 1; j <= line.Length; j++)
-                {
-                    matrix[row, j] = line[j - 1];
-                }
-
-                row++;
-            }
+            BuildMatrix(input, matrix, rowLength, columnLength);
 
             #region part one
 
             while (run == true)
             {
-                CopyMatrix(matrixCopy, matrix, input.Length + 2, input[0].Length + 2);
+                CopyMatrix(matrixCopy, matrix, rowLength, columnLength);
 
-                for (int i = 1; i <= input.Length; i++)
+                for (int i = 1; i <= rowLength - 2; i++)
                 {
-                    for (int j = 1; j <= input[0].Length; j++)
+                    for (int j = 1; j <= columnLength - 2; j++)
                     {
                         SeatAlteration(matrix,
                                        matrixCopy,
@@ -60,21 +44,18 @@ namespace Day9
                     }
                 }
 
-                run = CompareMatrices(matrix, matrixCopy, input.Length, input[0].Length);
+                run = CompareMatrices(matrix, matrixCopy, rowLength - 2, columnLength - 2);
 
-                CopyMatrix(matrix, matrixCopy, input.Length + 2, input[0].Length + 2);
+                CopyMatrix(matrix, matrixCopy, rowLength, columnLength);
             }
 
 
-            for (int i = 0; i < input.Length + 2; i++)
+            for (int i = 0; i < rowLength; i++)
             {
-                for (int j = 0; j < input[0].Length + 2; j++)
+                for (int j = 0; j < columnLength; j++)
                 {
                     if (matrix[i, j] == '#') countSeats++;
-                    Console.Write(matrix[i, j] + " ");
                 }
-
-                Console.WriteLine();
             }
 
             Console.WriteLine(countSeats);
@@ -155,6 +136,31 @@ namespace Day9
                 {
                     matrixOne[i, j] = matrixTwo[i, j];
                 }
+            }
+        }
+
+        private static void BuildMatrix(string[] input, char[,] matrix, int rowLength, int columnLength)
+        {
+            var row = 1;
+
+            // Building outer layer
+            for (int i = 0; i < rowLength; i++)
+            {
+                for (int j = 0; j < columnLength; j++)
+                {
+                    matrix[i, j] = '.';
+                }
+            }
+
+            // Building inner layer
+            foreach (var line in input)
+            {
+                for (int j = 1; j <= line.Length; j++)
+                {
+                    matrix[row, j] = line[j - 1];
+                }
+
+                row++;
             }
         }
     }
